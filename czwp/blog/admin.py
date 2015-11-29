57083faac40e051c555b1e19b14d06e84c535165
katrinP <atrit@seznam.cz>
2015-11-29 11:01:44 +0100

    reorganisation

--- czwp/blog/admin.py
+++ czwp/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    change_form_template = 'blog/admin/change_form.html'
+
+admin.site.register(Post, PostAdmin)
+
+
+
